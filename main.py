e6576fe986fb041a47f6e409ef6c5c3b0b21ac92
Noor <thenoorsacademy@gmail.com>
2026-08-13 21:34:16 +0400

    Add main menu with placeholder functions

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
-print("Hello, world!")
+from setup_database import create_database_and_table
+from student_operations import (
+    add_student,
+    view_students,
+    search_student_by_city,
+    update_student,
+    delete_student
+)
+
+def main_menu():
+    create_database_and_table()
+
+    while True:
+        print("\n")
+        print("=" * 50)
+        print("Student Management System")
+        print("=" * 50)
+        print("1. Add Student")
+        print("2. View Students")      
+        print("3. Search Students by City")
+        print("4. Update Student")
+        print("5. Delete Student")
+        print("6. Exit")
+
+        choice = input("Enter Choice: ")
+
+        if choice == "1":
+            add_student()
+        elif choice == "2":
+            view_students()
+        elif choice == "3":
+            search_student_by_city()
+        elif choice == "4":
+            update_student()
+        elif choice == "5":
+            delete_student()
+        elif choice == "6":
+            print("Thank you.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+main_menu()
+
 """
 student-management-mysql
 
---
+++ student_operations.py
@@ -0,0 +1,14 @@
+def add_student():
+    print("Add Student module coming soon")
+
+def view_students():
+    print("Vive Students module coming soon")
+
+def search_student_by_city():
+    print("Search Students by City module coming soon")
+
+def update_student():
+    print("Update Student module coming soon")
+
+def delete_student():
+    print("Delete Student module coming soon")
